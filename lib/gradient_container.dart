8eb462faa7f20b50a4bd01aa21c904aeb2c0791c
Venesseus <satriayusufrafaly@gmail.com>
2023-06-06 16:43:47 +0700

    Changing positional arguments in the constructor

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -20,7 +20,9 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(child: StyledText()),
+      child: const Center(
+        child: StyledText("Hello Flutter!!!"),
+      ),
     );
   }
 }
